f1d74a33804e3c2ab5ea9a1338e11a03e91e1789
xueqian li <xueqian.li@aiml.team>
2023-02-09 16:10:18 +1100

    merge xueqian with main

---
+++ on_laptop/model.py
@@ -0,0 +1,27 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torchvision import models
+
+
+class PenguinNet(nn.Module):
+    def __init__(self, embedding='vgg', hidden_layers=2, dim_k=64):
+        super().__init__()
+        if embedding == 'pretrain_vgg':
+            models.VGG11_BN_Weights(pretrained=True)
+        elif embedding == '':
+            self.conv1 = nn.Conv2d(3, 6, 5)
+            self.pool = nn.MaxPool2d(2, 2)
+            self.conv2 = nn.Conv2d(6, 16, 5)
+            self.fc1 = nn.Linear(16 * 5 * 5, 120)
+            self.fc2 = nn.Linear(120, 84)
+            self.fc3 = nn.Linear(84, 11)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = torch.flatten(x, 1) # flatten all dimensions except batch
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
--- on_laptop/steerDS.py
+++ on_laptop/steerDS.py
@@ -1,20 +1,68 @@
-import torch
+import os, glob
 import numpy as np
-from glob import glob
-from torchvision import transforms
-from torch.utils.data import Dataset, DataLoader
 import cv2
-from glob import glob
-from os import path
-import matplotlib.pyplot as plt
+import torch
+import torchvision.transforms.functional as transF
+
+from torchvision import transforms
+from torch.utils.data import Dataset
+
+
+# thank you https://discuss.pytorch.org/t/how-to-add-noise-to-mnist-dataset-when-using-pytorch/59745
+class AddGaussianNoise(object):
+    def __init__(self, mean=0., std=1.):
+        self.std = std
+        self.mean = mean
+        
+    def __call__(self, tensor):
+        return tensor + torch.randn(tensor.size()) * self.std + self.mean
+    
+    def __repr__(self):
+        return self.__class__.__name__ + '(mean={0}, std={1})'.format(self.mean, self.std)
+
+
+class CropData():
+    def __init__(self, xmin, ymin, width, height):
+        self.xmin = xmin
+        self.ymin = ymin
+        self.width = width
+        self.height = height
+        
+    def __call__(self, tensor):
+        return transF.crop(tensor, self.xmin, self.ymin, self.height, self.width)
+    
 
 class SteerDataSet(Dataset):
     
-    def __init__(self,root_folder,img_ext = ".jpg" , transform=None):
+    def __init__(self,root_folder,img_ext = ".jpg" , transform=None, mode='train', trial_file_name=None, seed=917):
+        torch.manual_seed(seed)
+        torch.cuda.manual_seed_all(seed)
+        np.random.seed(seed)
+        
         self.root_folder = root_folder
         self.transform = transform        
         self.img_ext = img_ext        
-        self.filenames = glob(path.join(self.root_folder,"*" + self.img_ext))            
+        # NOTE: get data for specific mode
+        if trial_file_name != None:
+            trials_fi = sorted([self.root_folder + '/' + line.rstrip('\n') for line in open(trial_file_name)])
+        else:
+            trials_fi = sorted(glob.glob(os.path.join(self.root_folder, '*')))
+        
+        self.filenames = []
+        for i in range(len(trials_fi)):
+            self.filenames.append(sorted(glob.glob(os.path.join(trials_fi[i], '*.jpg'))))
+        # NOTE: flatten list
+        self.filenames = [item for sublist in self.filenames for item in sublist]
+        
+        # NOTE: randomly choose 80% for training, 20% for validation
+        if mode != 'test':
+            if mode == 'train':
+                data_len = int(len(self.filenames) * 0.8)
+            elif mode == 'eval':
+                data_len = int(len(self.filenames) * 0.2)
+            fi_idx = np.random.choice(len(self.filenames), data_len, replace=False)
+            self.filenames = [self.filenames[index] for index in fi_idx]
+            
         self.totensor = transforms.ToTensor()
         
     def __len__(self):        
@@ -36,55 +84,4 @@ class SteerDataSet(Dataset):
         
         return sample
 
-
-def test():
-
-    # thank you https://discuss.pytorch.org/t/how-to-add-noise-to-mnist-dataset-when-using-pytorch/59745
-    class AddGaussianNoise(object):
-        def __init__(self, mean=0., std=1.):
-            self.std = std
-            self.mean = mean
-            
-        def __call__(self, tensor):
-            return tensor + torch.randn(tensor.size()) * self.std + self.mean
-        
-        def __repr__(self):
-            return self.__class__.__name__ + '(mean={0}, std={1})'.format(self.mean, self.std)
-
-        
-
-    transform = transforms.Compose(
-    [
-     transforms.ToTensor(),
-     transforms.CenterCrop(150),
-    # TODO: better crop
-    #  transforms.Resize(size=(48, 64)),
-     transforms.ColorJitter(brightness=0.5),
-     AddGaussianNoise(0., 0.04),
-     
-    #  transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-     ])
-     #TODO (maybe (if there's time)): flip (and flip angle)
-
-    ds = SteerDataSet("../dev_data/training_data",".jpg",transform)
-
-    print("The dataset contains %d images " % len(ds))
-
-    ds_dataloader = DataLoader(ds,batch_size=1,shuffle=False)
-    for S in ds_dataloader:
-        im = S["image"]    
-        y  = S["steering"]
-        
-        print(im.shape)
-        print(im.max(),im.min())
-
-        plt.imshow(im.detach().squeeze().permute(1,2,0).cpu().numpy().reshape(150,150,3))
-
-        plt.show()
-        print(y)
-        break
-
-
-
-if __name__ == "__main__":
-    test()
+    
